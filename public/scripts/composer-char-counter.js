fd7c5383c1ce216ea06188afb0ad7f31acd55490
Rick <rickywu209@gmail.com>
2022-03-30 01:32:46 +0000

    added tweet container along with icons

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,35 +1,33 @@
-$(document).ready(function() {
-  
-  // $('#tweet-text').click(function() {
-  //   console.log('clicked', this);
-  // })
-  // $('#tweet-text').blur(function() {
-  //   console.log('blur', this);
-  // })
-  // $('#tweet-text').keydown(function() {
-  //   console.log('keydown', this);
-  // })
-  // $('#tweet-text').keyup(function() {
-  //   console.log('keyup', this);
-  // })
-  // $('#tweet-text').keypress(function() {
-  //   console.log('keypress', this);
-  // })
-  // $('#tweet-text').change(function() {
-  //   console.log('change', this);
-  // })
-  // $('#tweet-text').input(function() {
-  //   console.log('input', this);
-  // })
-$('textarea').keyup(function() {
-  const keyCount = $(this).val().length;
-  const counterLog = $(this).parent().find(".counter") //traverses through DOM to parents
-  counterLog.val(140 - keyCount);
-  if (keyCount >= 0 && keyCount < 140) {
-    counterLog.css({"color": "#545149"});
-  } else {
-    counterLog.css({"color": "red"});
-  }
-})
-
+$(document).ready(function () {
+	// $('#tweet-text').click(function() {
+	//   console.log('clicked', this);
+	// })
+	// $('#tweet-text').blur(function() {
+	//   console.log('blur', this);
+	// })
+	// $('#tweet-text').keydown(function() {
+	//   console.log('keydown', this);
+	// })
+	// $('#tweet-text').keyup(function() {
+	//   console.log('keyup', this);
+	// })
+	// $('#tweet-text').keypress(function() {
+	//   console.log('keypress', this);
+	// })
+	// $('#tweet-text').change(function() {
+	//   console.log('change', this);
+	// })
+	// $('#tweet-text').input(function() {
+	//   console.log('input', this);
+	// })
+$('textarea').keyup(function () {
+		const keyCount = $(this).val().length;
+		const counterLog = $(this).parent().find('.counter'); //traverses through DOM to parents
+		counterLog.val(140 - keyCount);
+		if (keyCount >= 0 && keyCount < 140) {
+			counterLog.css({ color: '#545149' });
+		} else {
+			counterLog.css({ color: 'red' });
+		}
+	});
 });
