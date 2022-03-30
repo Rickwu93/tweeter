b2df8e051fcec439f93a30b5c2a504e634e2677d
Rick <rickywu209@gmail.com>
2022-03-30 21:08:23 +0000

    added dynamic ajax request for text box

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,68 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Fake data taken from initial-tweets.json
+const data = [
+	{
+		user: {
+			name: 'Newton',
+			avatars: 'https://i.imgur.com/73hZDYK.png',
+			handle: '@SirIsaac',
+		},
+		content: {
+			text: 'If I have seen further it is by standing on the shoulders of giants',
+		},
+		created_at: 1461116232227,
+	},
+	{
+		user: {
+			name: 'Descartes',
+			avatars: 'https://i.imgur.com/nlhLi3I.png',
+			handle: '@rd',
+		},
+		content: {
+			text: 'Je pense , donc je suis',
+		},
+		created_at: 1461113959088,
+	},
+];
+
+const createTweetElement = function (tweetObj) {
+  console.log(tweetObj.user.name)
+	let $tweet = $(`
+  <article class="tweet">
+				<header class="tweet-header">
+					
+						<img class="avatar" src=${tweetObj.user.avatars} width="50" height="50" />
+						<p>${tweetObj.user.name}</p>
+					
+					<h3 class="user-handle">${tweetObj.user.handle}</h3>
+				</header>
+				<section class="tweet-text">${tweetObj.content.text}</section>
+				<hr />
+				<footer class="tweet-footer">
+					<p>10 days ago</p>
+					<span class="icons">
+						<i class="fa-solid fa-flag"></i>
+						<i class="fa-solid fa-retweet"></i>
+						<i class="fa-solid fa-heart"></i>
+					</span>
+				</footer>
+      <article>
+      `);
+	return $tweet;
+};
+
+const renderTweets = function (tweets) {
+	// loops through tweets
+	// calls createTweetElement for each tweet
+	// takes return value and appends it to the tweets container
+	for (let tweetData of tweets) {
+		const $tweet = createTweetElement(tweetData);
+		$('.tweet-container').append($tweet);
+	}
+};
+$(document).ready(function() {
+  renderTweets(data);
+})
+
